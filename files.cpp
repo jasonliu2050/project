92dcd157b61bb33dd17ba374df06ab7934491698
jason <junliu99@hotmail.com>
2019-01-27 14:38:08 -0500

    add c++ code

--- files.cpp
+++ files.cpp
@@ -1,5 +1,30 @@
-jason
-bob
-steve
-# add comments
-end
+#include <iostream>
+#include <string>
+#include <vector>
+class base {
+public:
+	int age;
+};
+class d1 {
+public:
+	int d1;
+};
+
+void swap(int& a,int&b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int get() {
+	return 1;
+}
+
+int main() {
+	
+	for (int i = 0; i < 50; i++) {
+		cout << "i="<<i<<endl;
+		return 0;
+	}
+}
+
