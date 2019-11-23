85677d1426a3c02bc4da12efd02da9cafb750be2
jasonliu2050 <35857717+jasonliu2050@users.noreply.github.com>
2019-11-22 19:19:14 -0500

    Update ExploreDigitsDataset.py

--- ExploreDigitsDataset.py
+++ ExploreDigitsDataset.py
@@ -8,13 +8,6 @@ digits = datasets.load_digits()
 plt.figure(1, figsize=(4, 4))
 plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
 
-# images_and_labels = list(zip(digits.images, digits.target))
-# for index, (image, label) in enumerate(images_and_labels[:8]):
-#     plt.subplot(2, 4, index + 1)
-#     plt.axis('off')
-#     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-#     plt.title('%i' % label)
-#plt.title(digits.target[0])
 plt.title("Target: 0")
 print(digits.data.shape)
 print(digits.data[0].reshape(8,8))
