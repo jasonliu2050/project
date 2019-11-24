9dd38d7f71e953974f2e89be70bf82d042d486dd
jasonliu2050 <35857717+jasonliu2050@users.noreply.github.com>
2019-11-24 13:53:07 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,74 +1,87 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection  import cross_val_score, cross_val_predict
+import keras
+
 from sklearn import neighbors
 from sklearn.model_selection import train_test_split
-from sklearn import datasets, svm, metrics
-
-# Load dataset from online
-digits = datasets.load_digits()
-
-# Explore the dataset. Apply PCA by fitting the data with only 8 dimensions
-plt.figure()
+from sklearn import  metrics
+from keras.datasets import mnist
 from sklearn.decomposition import PCA
-pca = PCA(n_components= 8).fit(digits.data)
-# Transform the data using the PCA fit above
-X = pca.fit_transform(digits.data)
-y = digits.target
-plt.scatter(X[:, 0],X[:, 1], c=y, cmap="Paired")
-plt.colorbar()
-
-#To apply a classifier on this data, we can flat the image, to turn the data in a (samples, feature) matrix, or directly use the data.
-n_samples = len(digits.images)
-print("len=",n_samples)
-data = digits.images.reshape((n_samples, -1))
-print(data.shape)
-
-#create 3 classifiers, print Classification Report
+from keras.layers import Dense # Dense layers are "fully connected" layers
+from keras.models import Sequential # https://keras.io/models/sequential/
 from sklearn.linear_model import Perceptron
+
 classifiers = [
     ("Perceptron", Perceptron(tol=1e-3)),
-    ("KNeighborsClassifier", neighbors.KNeighborsClassifier(n_neighbors=2)),
-    ("SVC", svm.SVC(gamma=0.001))
+    ("KNeighborsClassifier", neighbors.KNeighborsClassifier(n_neighbors=2))
+
 ]
+image_vector_size = 28*28
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], image_vector_size)
+x_test = x_test.reshape(x_test.shape[0], image_vector_size)
+
+
+# Use 10000 training samples
+X, y = x_train[:10000], y_train[:10000]
+# USe 100 components
+pca = PCA(n_components = 100).fit(X)
+projected = pca.fit_transform(X)
+
+
+classifiers = [
+    ("Perceptron", Perceptron(tol=1e-3)),
+    ("KNeighborsClassifier", neighbors.KNeighborsClassifier(n_neighbors=2))
+
+]
+yy = []
+yy_ = []
 for name, clf in classifiers:
-    X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.35, random_state=0)
+    print("Train %s" % name)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=21)
     clf.fit(X_train, y_train)
-    predicted = clf.predict(X_test)
+    y_pred = clf.predict(X_test)
+    # yy_.append(1 - np.mean(y_pred == y_test))
+    yy.append(np.mean(y_pred == y_test))
 
-    print("Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(y_test, predicted)))
-    #print("Confusion matrix:\n%s" % metrics.confusion_matrix(y_test, predicted))
+    print("Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(y_test, y_pred)))
+    print("Confusion matrix:\n%s" % metrics.confusion_matrix(y_test, y_pred))
 
-# Showing how different algorithms perform on the hand-written digits dataset.
-plt.figure()
-heldout = [0.95, 0.90, 0.75, 0.50, 0.01]
-rounds = 20
-digits = datasets.load_digits()
-X, y = digits.data, digits.target
 
-classifiers = [
-    ("Perceptron", Perceptron(tol=1e-3)),
-    ("KNeighborsClassifier", neighbors.KNeighborsClassifier(n_neighbors=2)),
-    ("SVC", svm.SVC(gamma=0.001))
-]
-xx = 1. - np.array(heldout)
-for name, clf in classifiers:
-    print("training %s" % name)
-    rng = np.random.RandomState(42)
-    yy = []
-    for i in heldout:
-        yy_ = []
-        for r in range(rounds):
-            X_train, X_test, y_train, y_test = \
-                train_test_split(X, y, test_size=i, random_state=rng)
-            clf.fit(X_train, y_train)
-            y_pred = clf.predict(X_test)
-            yy_.append(1 - np.mean(y_pred == y_test))
-        yy.append(np.mean(yy_))
-    plt.plot(xx, yy, label=name)
-plt.legend(loc="upper right")
-plt.xlabel("Proportion train")
-plt.ylabel("Test Error Rate")
+num_classes = 10
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], image_vector_size)
+x_test = x_test.reshape(x_test.shape[0], image_vector_size)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+image_size = 784 # 28*28
+num_classes = 10 # ten unique digits
+
+model = Sequential()
+model.add(Dense(units=1024, activation='sigmoid', input_shape=(image_size,)))
+model.add(Dense(units=num_classes, activation='softmax'))
+
+print(model.summary())
+
+print("Please wait ... coming soon")
+model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=False, validation_split=.1)
+loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
+
+# accuracy = format(accuracy, '.3f')
+yy.append(accuracy)
+
+x = ['','Perceptron', 'KNN', 'Neural Network']
+xx =  [1, 2,3]
+
+x_pos =  [i for i, _ in enumerate(x)]
+
+plt.bar( xx, yy, color=["g","b","y"],width = 0.5)
+
+plt.ylabel("Accuracy")
+plt.title("Comparison of Machine learning Algorithm")
+
+plt.xticks(x_pos, x)
 
 plt.show()
-
